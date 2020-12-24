eeca664c33b8bf794da2b0c6a69d6dc47cf33a03
david02 <davidjerman3@gmail.com>
2020-12-24 21:12:16 +0100

    Initial commit

---
+++ arduino/weather_collector/weather_collector.ino
@@ -0,0 +1,63 @@
+#include "DHT.h"
+
+#define LIGHT_PIN A0
+#define WATER_PIN A1
+#define LED_ON 3
+#define LED_OFF 4
+#define RESET 5
+#define DHTPIN 2     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  pinMode(LIGHT_PIN, INPUT);
+  pinMode(WATER_PIN, INPUT);
+  pinMode(LED_ON, OUTPUT);
+  pinMode(LED_OFF, OUTPUT);
+  pinMode(RESET, OUTPUT);
+  digitalWrite(LED_ON, 1);
+  digitalWrite(LED_OFF, 0);
+  digitalWrite(RESET, 0);
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  delay(200);
+  double light_avgs = 0;
+  double water_avgs = 0;
+  for (int i = 0; i < 20; i++) {
+    double light_sum = 0;
+    double water_sum = 0;
+    for (int j = 0; j < 20; j++) {
+      light_sum += analogRead(LIGHT_PIN)/1024.0;
+      water_sum += analogRead(WATER_PIN)/256.0;
+      delay(25);
+    }
+    
+    water_avgs += water_sum/20.0;
+    light_avgs += light_sum/20.0;
+  }
+  double water_average_lvl = water_avgs/20.0;
+  double light_average_lvl = light_avgs/20.0;
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (isnan(h) || isnan(t)) {
+    digitalWrite(LED_ON, 0);
+    digitalWrite(LED_OFF, 1);
+    delay(2000);
+    digitalWrite(RESET, 1);
+  }
+  digitalWrite(LED_ON, 1);
+  digitalWrite(LED_OFF, 0);
+  Serial.print("light:[");
+  Serial.print(light_average_lvl);
+  Serial.print("],water:[");
+  Serial.print(water_average_lvl);
+  Serial.print("],temp:[");
+  Serial.print(t);
+  Serial.print("],humidity:[");
+  Serial.print(h);
+  Serial.println("]");
+}
---
+++ site/site.py
@@ -0,0 +1,41 @@
+import datetime
+from flask import Flask, render_template
+import sqlite3
+app = Flask(__name__)
+
+
+@app.route("/")
+def website():
+    date = datetime.datetime.now().strftime("%d.%m.%Y - %H:%M:%S")
+    temp, moisture, water, sunlight = get_data()
+    return render_template(template_name_or_list='index.html', date=date, temp=str(temp) + " °C",
+                           moisture=str(moisture) + "%", water=str(water) + " %", sunlight=str(sunlight) + " %")
+
+
+def get_data():
+    connection = sqlite3.connect('../dataGrabber/database.sqlite3')
+    cursor = connection.cursor()
+    cursor.execute(f'''SELECT light, water, temp, humidity FROM data
+                        ORDER BY year DESC, month DESC, day DESC, hour DESC, minute DESC''')
+    c = 0
+    light_avg = 0
+    water_avg = 0
+    temp_avg = 0
+    humidity_avg = 0
+    for data in cursor.fetchall():
+        light, water, temp, humidity = data
+        light_avg += light
+        water_avg += water
+        temp_avg += temp
+        humidity_avg += humidity
+        c += 1
+        if c == 6:
+            light_avg /= 6
+            water_avg /= 6
+            temp_avg /= 6
+            humidity_avg /= 6
+            return round(temp_avg, 1), round(humidity_avg, 1), round(water_avg*100, 1), round(light_avg*100, 1)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
