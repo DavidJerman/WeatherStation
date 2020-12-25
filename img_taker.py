5f15be9c545152a893baefbe4b8d3c4407bf7e41
david02 <davidjerman3@gmail.com>
2020-12-25 15:16:02 +0100

    Update.

--- img_taker.py
+++ img_taker.py
@@ -3,7 +3,6 @@ from time import sleep
 from PIL import Image
 from datetime import datetime
 import os
-from os.path import isfile
 
 
 camera = PiCamera()
@@ -13,13 +12,13 @@ while True:
                                      datetime.now().strftime("%M")
     for file in os.listdir("./static"):
         if file != "other":
-            os.remove(file)
+            os.remove("./static/" + file)
     name = "_" + year + "_" + month + "_" + day + "_" + hour + "_" + minute
 
     camera.start_preview()
     camera.capture(f"./static/view{name}.jpg")
     camera.stop_preview()
-    picture = Image.open(f"./images/view{name}.jpg")
-    picture.save(f"./images/view_compressed{name}.jpg", optimize=True, quality=40)
+    picture = Image.open(f"./static/view{name}.jpg")
+    picture.save(f"./static/view_compressed{name}.jpg", optimize=True, quality=40)
     os.remove(f'./static/view{name}.jpg')
     sleep(61)
