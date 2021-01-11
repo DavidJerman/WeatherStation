33a7b1845daffe819929349d0b114d3ffa67291c
david02 <davidjerman3@gmail.com>
2021-01-11 22:53:43 +0100

    Rearranged file structure, added a temperature plot on the website.

--- img_taker.py
+++ img_taker.py
@@ -12,16 +12,15 @@ while True:
     year, month, day, hour, minute = datetime.now().strftime("%Y"), datetime.now().strftime("%m"),\
                                      datetime.now().strftime("%d"), datetime.now().strftime("%H"),\
                                      datetime.now().strftime("%M")
-    for file in os.listdir("static"):
-        if file != "other":
-            os.remove("./static/" + file)
+    for file in os.listdir("static/imgs"):
+        os.remove("./static/imgs/" + file)
     name = "_" + year + "_" + month + "_" + day + "_" + hour + "_" + minute
 
     camera.start_preview()
-    camera.capture(f"./static/view{name}.jpg")
+    camera.capture(f"./static/imgs/view{name}.jpg")
     camera.stop_preview()
-    picture = Image.open(f"./static/view{name}.jpg")
-    picture.save(f"./static/view_compressed{name}.jpg", optimize=True, quality=40)
-    os.remove(f'./static/view{name}.jpg')
+    picture = Image.open(f"./static/imgs/view{name}.jpg")
+    picture.save(f"./static/imgs/view_compressed{name}.jpg", optimize=True, quality=60)
+    os.remove(f'./static/imgs/view{name}.jpg')
     print("Image taken: " + str(c))
     sleep(61)
--- site.py
+++ site.py
@@ -1,8 +1,12 @@
 import datetime
 from pytz import timezone
 from flask import Flask, render_template
+from PIL import Image
 import sqlite3
 import os
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
 app = Flask(__name__)
 
 
@@ -13,12 +17,15 @@ def website():
     date = date.strftime("%d-%m-%Y %H:%M:%S")
     temp, moisture, water, sunlight = get_data()
     img_name = ""
-    for file in os.listdir('static'):
-        if file != "other":
-            img_name = file
+    plot_name = ""
+    create_plot()
+    for file in os.listdir('static/imgs'):
+        img_name = file
+    for file in os.listdir('static/plots'):
+        plot_name = file
     return render_template(template_name_or_list='index.html', date=date, temp=str(temp) + " °C",
                            moisture=str(moisture) + "%", water=str(water) + " %", sunlight=str(sunlight) + " %",
-                           image=os.path.join('static', img_name))
+                           image=os.path.join('static/imgs', img_name), plot=os.path.join('static/plots', plot_name))
 
 
 def get_data():
@@ -43,7 +50,47 @@ def get_data():
             water_avg /= 6
             temp_avg /= 6
             humidity_avg /= 6
+            connection.close()
             return round(temp_avg, 1), round(humidity_avg, 1), round(water_avg*100, 1), round(light_avg*100, 1)
+
+
+def create_plot():
+    year, month, day, hour, minute = datetime.datetime.now().strftime("%Y"), datetime.datetime.now().strftime("%m"), \
+                                     datetime.datetime.now().strftime("%d"), datetime.datetime.now().strftime("%H"), \
+                                     datetime.datetime.now().strftime("%M")
+    for file in os.listdir("static/plots"):
+        os.remove("./static/plots/" + file)
+    name = "_" + year + "_" + month + "_" + day + "_" + hour + "_" + minute
+
+    connection = sqlite3.connect("dataGrabber/database.sqlite3")
+    result = connection.execute("SELECT temp FROM data")
+    temps = []
+    for temp in result:
+        temps.append(temp[0])
+
+    plt.plot(slope(temps, 100))
+    plt.title("Temperatures")
+    plt.ylabel("Temperature [C]")
+    plt.xlabel("Measurement No.")
+    plt.savefig(f"./static/plots/plot{name}.jpg")
+    plt.close()
+
+    picture = Image.open(f"./static/plots/plot{name}.jpg")
+    picture.save(f"./static/plots/plot_compressed{name}.jpg", optimize=True, quality=60)
+    os.remove(f'./static/plots/plot{name}.jpg')
+    print("Plot created")
+    connection.close()
+
+
+def slope(source, slope_range):
+    avgs = []
+    for i in range(int(slope_range/2), len(source) - int(slope_range/2) - 1):
+        sum = 0
+        for j in range(int(slope_range/2)):
+            sum += float(source[-j + i])
+            sum += float(source[j + i])
+        avgs.append(sum/slope_range)
+    return avgs
 
 
 if __name__ == "__main__":
